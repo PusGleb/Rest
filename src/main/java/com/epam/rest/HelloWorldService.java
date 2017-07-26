8818e3a27d2242c49e9dfef19441f219dbcf0d95
Hleb_Pus <Hleb_Pus@epam.com>
2017-07-26 15:10:11 +0300

    First commit

---
+++ src/main/java/com/epam/rest/HelloWorldService.java
@@ -0,0 +1,25 @@
+package com.epam.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by Hleb_Pus on 7/26/2017.
+ */
+
+@Path("/hello")
+public class HelloWorldService {
+
+
+    @GET
+    @Path("/{param}")
+    public Response getMsg(@PathParam("param") String msg) {
+
+        String output = "Jersey say : " + msg;
+
+        return Response.status(200).entity(output).build();
+
+    }
+}
